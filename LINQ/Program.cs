580ff995c788c87501b6c38dce61c6c98646dfe0
Daniel Page <dpage105@gmail.com>
2025-04-30 16:03:22 -0500

    Created list of games; Sorted games by length using LINQ; Printed sorted list to console.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gamesList = new List<string>() {"Super Mario Bros.", "Quake", "Sonic the Hedgehog", "Final Fantasy", "Dragon Quest"};
+            
+            var sortedGamesList = gamesList.OrderBy(x => x.Length).ToList();
+            
+            Console.WriteLine("List of great games:");
+            Console.WriteLine("-----------");
+            sortedGamesList.ForEach(Console.WriteLine);
         }
     }
 }
